d020261ee7b6cfb86e9573e8db33ca70e528eaa3
Yuriy <yuriy.l@matic.com>
2023-03-09 12:31:41 +0200

    ADD LAYERS

--- Firmware/code.py
+++ Firmware/code.py
@@ -10,6 +10,7 @@ from kmk.modules.layers import Layers
 from kmk.modules.encoder import EncoderHandler
 from kmk.modules.tapdance import TapDance
 from kmk.modules.mouse_keys import MouseKeys
+from kmk.modules.sticky_mod import StickyMod
 from kmk.extensions.RGB import RGB
 from midi import Midi
 
@@ -19,8 +20,9 @@ keyboard = KMKKeyboard()
 encoders = EncoderHandler()
 tapdance = TapDance()
 mouse_keys = MouseKeys()
-tapdance.tap_time = 150
-keyboard.modules = [layers, encoders, tapdance, mouse_keys]
+sticky_mod = StickyMod() # TODO: Remove it
+tapdance.tap_time = 100
+keyboard.modules = [layers, encoders, tapdance, mouse_keys, sticky_mod]
 
 # SWITCH MATRIX
 keyboard.col_pins = (board.D3, board.D4, board.D5, board.D6)
@@ -32,7 +34,7 @@ encoders.pins = ((board.A2, board.A1, board.A0, False), (board.SCK, board.MISO, 
 
 # EXTENSIONS
 rgb_ext = RGB(pixel_pin = board.D10, num_pixels=4, hue_default=50)
-midi_ext = Midi()
+midi_ext = Midi() # TODO: Remove it
 keyboard.extensions.append(rgb_ext)
 keyboard.extensions.append(midi_ext)
 keyboard.debug_enabled = False
@@ -43,27 +45,23 @@ LAYER2 = KC.TO(1)
 LAYER3 = KC.TO(2)
 
 #******** Layer 1 *******
-# MACROS ROW 1
-UNDO_L3  = KC.TD(KC.LCMD(KC.Z), LAYER3)
-DEL_LINE = KC.LCMD(KC.D)
-SEL_ALL = KC.LCMD(KC.A)
-REDO  = KC.LCMD(KC.Y)
 
-# MACROS ROW 2
-ALT_TAB_L2 = KC.TD(KC.LALT(KC.TAB), LAYER2)
-CLEAR = simple_key_sequence([KC.LCMD(KC.LSFT(KC.BSPC))])
-INSPECT = simple_key_sequence([KC.LCMD(KC.LALT(KC.I))])
-BROWSER_PROD = simple_key_sequence([KC.LALT(KC.SPACE), KC.MACRO_SLEEP_MS(100), send_string('Google Chrome'),KC.ENTER, KC.LCMD(KC.T), KC.LCMD(KC.L), KC.MACRO_SLEEP_MS(100), send_string('https://ds-live.matic.sdm.network/admin'), KC.ENTER])
-
-# MACROS ROW 3
+# Layer 1 MACROS ROW 1
 ESCAPE_L1 = simple_key_sequence([KC.ESC, LAYER1])
 MOUSE_LEFT_CLICK = KC.MB_LMB
-PASTE = KC.LCMD(KC.V)
-TD_PASTE_MUTE = KC.TD(PASTE, KC.MUTE)
+TD_PASTE_MUTE = KC.TD(KC.LCMD(KC.V), KC.MUTE)
 LOCK = simple_key_sequence([KC.LCTRL(KC.LCMD(KC.Q)), KC.MACRO_SLEEP_MS(400), KC.ESCAPE])
 
-ZOOM_IN = KC.LCMD(KC.EQUAL)
-ZOOM_OUT = KC.LCMD(KC.MINUS)
+# Layer 1 MACROS ROW 2
+ALT_TAB_L2 = KC.TD(KC.LALT(KC.TAB), LAYER2)
+CYCLE_WINDOW = KC.LCMD(KC.GRAVE)
+INSPECT = simple_key_sequence([KC.LCMD(KC.LALT(KC.I))])
+CMD_TAB = KC.LCMD(KC.TAB) #KC.SM(KC.TAB, KC.LCMD)
+
+# Layer 1 MACROS ROW 3
+UNDO  = KC.LCMD(KC.Z)
+DEL_LINE = KC.LCMD(KC.D)
+REDO  = KC.LCMD(KC.Y)
 
 # CUSTOM KEYS
 def on_copy(*args, **kwargs):
@@ -88,6 +86,7 @@ def on_layer3(*args, **kwargs):
 LAYER3.after_release_handler(on_layer3)
 
 #******** Layer 2 *******
+#########################
 
 _______ = KC.TRNS
 xxxxxxx = KC.NO
@@ -97,30 +96,32 @@ xxxxxxx = KC.NO
 keyboard.keymap = [
     # Layer 1
     [
-        ESCAPE_L1,  MOUSE_LEFT_CLICK, TD_PASTE_MUTE, LOCK,
-        ALT_TAB_L2, CLEAR,            INSPECT,       BROWSER_PROD,
-        UNDO_L3,    DEL_LINE,         SEL_ALL,       REDO,
+        ESCAPE_L1,              MOUSE_LEFT_CLICK, TD_PASTE_MUTE, LOCK,
+        ALT_TAB_L2,             CYCLE_WINDOW,     INSPECT,       CMD_TAB,
+        KC.TD(_______, LAYER3), UNDO,             DEL_LINE,      REDO,
     ],
     # Layer 2
     [
-        ESCAPE_L1, xxxxxxx, xxxxxxx,
-        xxxxxxx,   xxxxxxx, xxxxxxx,
-        xxxxxxx,   xxxxxxx, xxxxxxx,
+        ESCAPE_L1,             KC.N1, KC.N2, KC.N3,
+        KC.TD(KC.N0, LAYER2),  KC.N4, KC.N5, KC.N6,
+        KC.TD(KC.DOT, LAYER3), KC.N7, KC.N8, KC.N9,
     ],
     # Layer 3
     [
-        ESCAPE_L1, xxxxxxx, xxxxxxx,
-        xxxxxxx,   xxxxxxx, xxxxxxx,
-        xxxxxxx,   xxxxxxx, xxxxxxx,
+        ESCAPE_L1,              xxxxxxx,             xxxxxxx,             xxxxxxx,
+        KC.TD(_______, LAYER2), xxxxxxx,             xxxxxxx,             xxxxxxx,
+        KC.TD(_______, LAYER3), KC.MEDIA_PREV_TRACK, KC.MEDIA_PLAY_PAUSE, KC.MEDIA_NEXT_TRACK,
     ]
 ]
+
+ZOOM_IN = KC.LCMD(KC.EQUAL)
+ZOOM_OUT = KC.LCMD(KC.MINUS)
 
 encoders.map = [
                  ((KC.VOLD, KC.VOLU, KC.MUTE),  (ZOOM_OUT, ZOOM_IN, KC.RGB_TOG)),
                  ((KC.VOLD, KC.VOLU, KC.MUTE),  (ZOOM_OUT, ZOOM_IN, KC.RGB_TOG)),
                  ((KC.VOLD, KC.VOLU, KC.MUTE ), (ZOOM_OUT, ZOOM_IN, KC.RGB_TOG)),
                 ]
-
 
 if __name__ == '__main__':
     keyboard.go()
